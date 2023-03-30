342efb4cd943b8768e0f431873bf7e88094497eb
Jai Kushwaha <kushwahajai74>
2023-03-30 17:30:09 +0530

    completed basic server using node.js

--- server.js
+++ server.js
@@ -2,11 +2,56 @@ const http = require("http");
 
 const port = 8081;
 
+const toDoList = ["Complete node byte", "Play Cricket"];
 http
-  .createServer((require, response) => {
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<h1> Hello, this is from my server</h1>");
-    response.end();
+  .createServer((req, res) => {
+    const { method, url } = req;
+
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(toDoList.toString());
+      } else if (method === "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let newToDo = toDoList;
+            newToDo.push(body.item);
+            console.log(newToDo);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+            for (let i = 0; i < toDoList.length; i++) {
+              if (toDoList[i] === deleteThis) {
+                toDoList.splice(i, 1);
+                break;
+              }
+            }
+          });
+      } else {
+        res.writeHead(501);
+      }
+    } else {
+      res.writeHead(404);
+    }
+    res.end();
   })
   .listen(port, () => {
     console.log(`Nodejs server started at port ${port}`);
